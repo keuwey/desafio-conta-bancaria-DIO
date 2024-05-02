27cbb0e2673a6b012cf2111d9898686ea8ef9025
danielcostadev <danielcostamarketing@gmail.com>
2024-05-02 00:15:37 -0300

    fix: Listar Usuários dados de endereço separados

--- business/usuario.py
+++ business/usuario.py
@@ -1,36 +1,47 @@
 import csv
 import re
 import os
-from pathlib import Path  
+from pathlib import Path
+
 
 class Usuario:
-    def __init__(self, nome: str, data_nascimento: str, cpf: str, endereco: str):
+    def __init__(self, nome: str, data_nascimento: str, cpf: str, endereco_logradouro: str, endereco_numero: str, endereco_bairro: str, endereco_cidade: str):
         if not re.match(r"\d{2}/\d{2}/\d{4}", data_nascimento):
             raise ValueError("Data de nascimento inválida")
         if not re.match(r"\d{11}", cpf):
             raise ValueError("CPF inválido")
-        if not re.match(r".+, \d+ - .+ - .+/\w{2}", endereco):
-            raise ValueError("Endereço inválido")
+        if not re.match(r".+", endereco_logradouro):
+            raise ValueError("Informação Incorreta!")
+        if not re.match(r"\d+", endereco_numero):
+            raise ValueError("Número incorreto!")
+        if not re.match(r".+", endereco_bairro):
+            raise ValueError("Número inválido!")
+        if not re.match(r".+/\w{2}", endereco_cidade):
+            raise ValueError("Informação Incorreta. Ex: São Paulo/SP!")
+
         self.nome = nome
         self.data_nascimento = data_nascimento
         self.cpf = cpf
-        self.endereco = endereco
-        cpf_list = [] # lista para armezar cpfs cadastrados, para posterior verificação
-        
+        self.endereco_logradouro = endereco_logradouro
+        self.endereco_numero = endereco_numero
+        self.endereco_bairro = endereco_bairro
+        self.endereco_cidade = endereco_cidade
+        cpf_list = []  # lista para armezar cpfs cadastrados, para posterior verificação
+
         # (Daniel Costa: Comecei a resolução ás 00:30 - 01/05/2024)
 
         # Definição do sistema de arquivos (Reformulado por Daniel Costa)
         self.caminho_data = Path('data').absolute()
         # Linha para evitar repetição extensa de código
         self.arquivo_usuario = Path(str(self.caminho_data) + '\\usuarios.csv')
-        
+
         # Verfica se é um diretório
         if not self.caminho_data.is_dir():
-           # Cria o diretório, em caso de existência evita erros através do 'exists_ok=True'
-           self.caminho_data.mkdir(exist_ok=True)
-           # Atribui permissões ao diretório
-           self.caminho_data.chmod(0o000777)
-        
+            # Cria o diretório, em caso de existência evita erros através do 'exists_ok=True'
+            self.caminho_data.mkdir(exist_ok=True)
+            # Atribui permissões ao diretório
+            self.caminho_data.chmod(0o000777)
+
         # Se o arquivo existir, realiza as intruções abaixo: abre o arquivo e lê campo por campo
         if self.arquivo_usuario.exists():
             with open(str(self.arquivo_usuario), "r") as file:
@@ -38,17 +49,19 @@ class Usuario:
                 for row in reader:
                     # Adiciona CPF a lista de cpfs para comparação
                     cpf_list.append(row[2])
-            # Se o CPF não estiver na lista adiciona novo Usuário        
+            # Se o CPF não estiver na lista adiciona novo Usuário
             if self.cpf not in cpf_list:
                 # Faz a inclusão das novas informações caso o cpf do usuário não exista na lista
                 with open(str(self.arquivo_usuario), "a", newline="") as file:
                     writer = csv.writer(file)
-                    writer.writerow([self.nome, self.data_nascimento, self.cpf, self.endereco])
+                    writer.writerow([self.nome, self.data_nascimento, self.cpf, self.endereco_logradouro,
+                                    self.endereco_numero, self.endereco_bairro, self.endereco_cidade])
                     print("Usuário " + str(self.nome) + " criado com sucesso!")
-            else: 
-                print("Não foi possível adicionar usuário, CPF: "+ str(self.cpf) +" já cadastrado!")
-                
-        # Senão, caso o arquivo não exista realiza as instruções abaixo:        
+            else:
+                print("Não foi possível adicionar usuário, CPF: " +
+                      str(self.cpf) + " já cadastrado!")
+
+        # Senão, caso o arquivo não exista realiza as instruções abaixo:
         else:
             # Cria o arquivo usuarios.csv
             self.arquivo_usuario.touch()
@@ -56,5 +69,6 @@ class Usuario:
             self.arquivo_usuario.chmod(0o000777)
             # Após criar o arquivo faz a inclusão das informações
             with open(str(self.arquivo_usuario), "w", newline="") as file:
-                    writer = csv.writer(file)
-                    writer.writerow([self.nome, self.data_nascimento, self.cpf, self.endereco])
+                writer = csv.writer(file)
+                writer.writerow(
+                    [self.nome, self.data_nascimento, self.cpf, endereco_logradouro, endereco_numero, endereco_bairro, endereco_cidade])
--- modules/listar_usuario.py
+++ modules/listar_usuario.py
@@ -6,9 +6,11 @@ from business.usuario import Usuario
 data = mountDir('data')
 usuarios = mountFile('usuarios')
 
+
 def ListarUsuarios():
-    with open(str(data) +'\\'+str(usuarios), 'r') as file:
+    with open(str(data) + '\\'+str(usuarios), 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            nome, data_nascimento, cpf, endereco = row
-            print('Nome: '+str(nome)+', Data de Nascimento: '+str(data_nascimento)+', CPF: '+str(cpf)+', Endereço:' +str(endereco))
+            nome, data_nascimento, cpf, endereco_logradouro, endereco_numero, endereco_bairro, endereco_cidade = row
+            print('Nome: '+str(nome)+', Data de Nascimento: '+str(data_nascimento)+', CPF: '+str(cpf) +
+                  ', Endereço: ' + str(endereco_logradouro) + ', ' + str(endereco_numero) + ' - ' + str(endereco_bairro) + ' - ' + str(endereco_cidade))
